cc73ccf8fdff676f6de8d40568249fb7b19dcb9f
Gaspard-M <151040265+Gaspard-M@users.noreply.github.com>
2024-04-18 19:26:48 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,13 @@
+#ifndef PROJET_MAIN_H
+#define PROJET_MAIN_H
+
+
+typedef struct {
+    char * name_column;
+    int taille_physique_tableau;
+    int taille_logique_tableau;
+    int * donnees;
+}COLUMN;
+
+#endif //PROJET_MAIN_H
 
