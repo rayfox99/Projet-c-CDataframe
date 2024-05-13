148ceb10808f991008a8a4d8a815a2675637e5b3
Gaspard-M <151040265+Gaspard-M@users.noreply.github.com>
2024-05-13 08:28:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,30 +2,71 @@
 #include "main.h"
 #include "stdlib.h"
 #include "string.h"
-
-
-COLUMN * create_column(char *title) {
-    COLUMN *col = (COLUMN *) malloc((sizeof(COLUMN)));
-    col->taille_physique_tableau = 0;
-    col->taille_logique_tableau = 0;
-    col->donnees = NULL;
-    col->name_column = (char *) malloc((strlen(title) + 1) * sizeof(char));
-    strcpy(col->name_column, title);
-    return col;
-}
-
-int inservalue (COLUMN *nom, int valeur){
-    if (nom->donnees == NULL){
-        nom->donnees = malloc(256*sizeof(int));
-    }
-    nom->donnees = &valeur;
-}
+#include "column.h"
 
 int main() {
-    COLUMN * colonne1;
-    char * nom;
-    nom="colonne1";
-    colonne1= create_column(nom);
-
+    char nom[20];
+    DATAFRAME *cdataframe;
+    COLUMN *colonne;
+    int créé = 0;
+    int menu = 1;
+    int call;
+    while (menu == 1) {
+        while (créé == 0) {
+            printf("Liste des fonctions :\n[0] Créer un dataframe\n[1] : sortir du programme\n");
+            scanf("%d", &call);
+            if (call == 0) {
+                cdataframe = create_dataframe();
+                créé = 1;
+            }
+            if (call == 1) {
+                menu = 0;
+                créé = 1;
+            }
+        }
+        while(menu == 1) {
+            printf("\nListe des fonctions :\n[0] : sortir du programme\n[1] : Créer une colonne\n[2] : Imprimer le dataframe\n[3] : Ajouter une valeur à une colonne\n");
+            scanf("%d", &call);
+            if (call == 0) {
+                menu = 0;
+            }
+            if (call == 1) {
+                printf("Entrez le nom de votre colonne : ");
+                scanf("%s", &nom);
+                colonne=create_column(nom);
+                ajouter_colonne_au_dataframe(colonne,cdataframe);
+                printf("\n%d",cdataframe->taillelogique);
+            }
+            if (call==2){
+                print_dataframe(*cdataframe);
+            }
+            if (call==3){
+                int i;
+                int value;
+                printf("sélectionner une colonne : \n");
+                for (i=0; i < cdataframe->taillelogique; i++){
+                    printf("[%d] : %s\n", i, cdataframe->donnees[i]->name_column);
+                }
+                scanf("%d", &i);
+                printf("Tapez la valeur à ajouter à la colonne '%s' : ", cdataframe->donnees[i]->name_column);
+                scanf("%d", &value);
+                inservalue(cdataframe->donnees[i], value);
+            }
+        }
+    }
     return 0;
 }
+
+/*
+    char * nom;
+    int o;
+    scanf("%d", &o);
+    nom="colonne1";
+    colonne = create_column(nom);
+    printf("%s", colonne->name_column);
+    cdataframe->donnees[0] = colonne;
+    inservalue(colonne, o);
+    print_col(colonne);
+    return 0;
+}
+*/
